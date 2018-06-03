71e653ae15980bc6c13bbf5156b9db8035551287
Logan Doose <ldoose@live.com>
2018-06-02 20:04:17 -0700

    added mkdir to things

--- server/linux-or-macos/install_required_stuff.sh
+++ server/linux-or-macos/install_required_stuff.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd ..
 echo "To install Crunchycord, you need node.js and Git installed. If it is, just wait and let the magic do it's stuff."
+mkdir node_modules
 npm install pm2 -g
 npm install discord-rich-presence
 echo "Everything should work now, just open start_crunchycord and you should be good to go"
--- server/windows/install_required_stuff.bat
+++ server/windows/install_required_stuff.bat
@@ -1,8 +1,8 @@
 @echo off
 cd ..
 echo To install Crunchycord, you need node.js and Git installed. If it is, just wait and let the magic do it's stuff.
-npm install pm2 -g
 mkdir node_modules
+npm install pm2 -g
 npm install discord-rich-presence
 echo Everything should work now, just open start_crunchycord and you should be good to go.
 pause
